83a65f40a804da7e2352d63d4652fe5e4c44c5b9
Tarus622 <davideoliveira622@gmail.com>
2023-12-04 11:36:39 -0300

    First commit

---
+++ src/createWallet.js
@@ -0,0 +1,31 @@
+// Importando as dependências
+const bip32 = require('bip32');
+const bip39 = require('bip39');
+const bitcoin = require('bitcoinjs-lib');
+
+// Definir a rede
+const network = bitcoin.networks.testnet;
+
+// Derivação de carteiras HD
+const path = `m/49'/1'/0'/0`;
+
+// Criando o mnemonic para a seed
+let mnemonic = bip39.generateMnemonic();
+const seed = bip39.mnemonicToSeedSync(mnemonic);
+
+// Criando a raiz da carteira HD
+let root = bip32.fromSeed(seed, network);
+
+// Criando uma conta - par pvt-pub keys
+let account = root.derivePath(path);
+let node = account.derive(0).derive(0);
+
+let btcAddress = bitcoin.payments.p2pkh({
+    pubkey: node.publicKey,
+    network: network,
+}).address;
+
+console.log("Carteira gerada");
+console.log("Endereço: ", btcAddress);
+console.log("Chave privada: ", node.toWIF());
+console.log("Seed: ", mnemonic);
